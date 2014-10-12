2e4d592729e8fec3a6254644b3cbb1d35cf8c316
Etcoff <etcoffa@amazon.com>
2014-10-11 22:13:00 -0700

    checking progress of functionality

--- allieetcoff.js
+++ allieetcoff.js
@@ -14,7 +14,7 @@ $('.inline-popups').magnificPopup({
 });
 
 // FOR RETINA DISPLAY
-// $('.image-link').magnificPopup({ 
+// $('.image-link').magnificPopup({
 //   type: 'image',
 
 //   retina: {
@@ -22,3 +22,24 @@ $('.inline-popups').magnificPopup({
 //   }
 
 // });
+
+// FOR GALLERY ITEM
+
+$('div').magnificPopup({
+  delegate: 'a',
+  gallery:{
+    enabled:true},
+    callbacks: {
+
+    buildControls: function() {
+      // re-appends controls inside the main container
+      this.contentContainer.append(this.arrowLeft.add(this.arrowRight));
+    }
+
+  }
+  //   arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+ 	//   tPrev: 'Previous (Left arrow key)', // title for left button
+  //   tNext: 'Next (Right arrow key)', // title for right button
+  //   tCounter: '<span class="mfp-counter">%curr% of %total%</span>'
+  // }
+});
