9e9cb0e7d4fc625d8b3b917130f55df048d5f34f
Etcoff <etcoffa@amazon.com>
2014-10-14 19:30:16 -0700

    slick working but still buggy

--- allieetcoff.js
+++ allieetcoff.js
@@ -38,3 +38,36 @@ $('.gallery').magnificPopup({
 
   }
 });
+
+$('.project-image').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
